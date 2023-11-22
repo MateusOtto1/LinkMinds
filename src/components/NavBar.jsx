6ff555bb999830537aec37c0e597427886d830ea
Mateus <mateus-botto@educar.rs.gov.br>
2023-11-21 21:32:27 -0300

    Ajustes tela de cadastro 100%

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,6 +29,7 @@ import interessesIcon from "../imagens/pessoas.svg";
 const NavBar = () => {
     const [nome, setNome] = useState('');
     const [usuarios, setUsuarios] = useState({});
+    const [cadastro, setCadastro] = useState({});
     const [flag, setFlag] = useState(0);
     const bodyfalso = document.getElementById("bodyfalso");
     const nav = document.getElementById("navdentro");
@@ -121,6 +122,21 @@ const NavBar = () => {
         };
         getUsuario();
     }, [nome == '']);
+
+    useEffect(() => {
+        const getCadastro = async () => {
+            const token = Cookies.get('token');
+            const headers = {
+                "x-access-token": token
+            }
+            const response = await axios.get('https://server-link-minds.vercel.app/usuarioInfo', { headers });
+            setCadastro(response.data.apelido);
+            if (cadastro == "") {
+                navigate('/Cadastro');
+            }
+        };
+        getCadastro();
+    }, [cadastro == ""]);
 
     function handleClickHome() {
         setHome(true);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,8 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { AuthGoogleContext } from "../contexts/authGoogle";
-import axios from "axios";
-import Cookies from 'js-cookie';
-import { useEffect, useState } from "react";
 
 export const PrivateRoutes = () => {
   const { signed } = useContext(AuthGoogleContext);  
-  const [cadastro, setCadastro] = useState({});
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const getCadastro = async () => {
-      const token = Cookies.get('token');
-      const headers = {
-        "x-access-token": token
-      }
-      const response = await axios.get('https://server-link-minds.vercel.app/usuarioInfo', { headers });
-      setCadastro(response.data);
-      if (cadastro.apelido == "") {
-        navigate('/Cadastro');
-      }
-    };
-    getCadastro();
-  }, [cadastro.apelido == '']);
   return signed ? <Outlet /> : <Navigate to="/" />;
 };
