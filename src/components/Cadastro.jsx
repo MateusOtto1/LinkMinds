8ca0a251f0807dc0942fb0cc2d40eb98fbd97027
Mateus <mateus-botto@educar.rs.gov.br>
2023-11-21 22:59:10 -0300

    Teste de ajuste

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -24,7 +24,7 @@ const Cadastro = () => {
                 "x-access-token": token
             }
             const response = await axios.get('https://server-link-minds.vercel.app/usuarioInfo', { headers });
-            setUsuarios(await response.data);
+            setUsuarios(response.data);
             const response2 = await axios.get('https://server-link-minds.vercel.app/listaInteresse', { headers });
             setListaInteresse(response2.data);
             if (pesquisaInteresse === '') {
@@ -83,7 +83,7 @@ const Cadastro = () => {
             navigate('/');
         }
     };
-    if (usuarios.apelido != "") {
+    if (usuarios.apelido != null && usuarios.apelido != '' && usuarios.apelido != undefined) {
         navigate('/LinkMinds');
     } else {
         return (
